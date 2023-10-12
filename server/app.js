947a2d58087aaf5df9fb00d1454910489b247e60
Segebari <segebarifred@gmail.com>
2023-10-12 19:34:05 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,9 +5,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-
-
 const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    console.log(res)
+    return res.status(234).send('My First MERN Stack App')
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
