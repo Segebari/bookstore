a09d4cfe9620b5f39df146e3aff03e9d67541dd1
Segebari <segebarifred@gmail.com>
2023-10-13 15:35:08 +0100

    update

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { request, response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 
 dotenv.config();
@@ -7,11 +9,66 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//  mongoDb key
+
+const mongoDBURL =
+    'mongodb+srv://fredzugs:iotrFaMYSJIj9A5P@bookstoremern.u8lvi97.mongodb.net/books-collection?retryWrites=true&w=majority' 
+
+
+
+// Middleware for parsing request body
+app.use(express.json());
+
 app.get('/', (req, res) => {
     console.log(res)
     return res.status(200).send('My First MERN Stack App')
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+// Route for Save a new book
+app.post('/books', async (req, res) => {
+    try {
+        if (
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publisherYear
+        ) {
+            return res.status(400).send({
+                message: 'Send all requred fields: author, publisherYear',
+            });
+        }
+
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publisherYear: req.body.publisherYear,
+        };
+
+        const book = await Book.create(newBook);
+        res.status(201).send(book); // Return the newly created book
+    } catch(error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message});
+    }
+});
+
+
+// Route for GET ALL Books from database
+app.get();
+
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+
+    console.log('App  is connected to database');
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+    
+  })
+
+  .catch((error) => {
+    console.log(error);
   });
