e882f25234ac1706a5443374e7a9028ec7b19f0b
Segebari <segebarifred@gmail.com>
2023-10-13 16:20:21 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -69,6 +69,22 @@ app.get('/books', async (req,res) => {
 });
 
 
+// Route for GET One Book from database by ID
+app.get('/books', async (req,res) => {
+    try {
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
