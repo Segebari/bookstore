31677f12165a99e767228f83688232bb238b0db7
Segebari <segebarifred@gmail.com>
2023-10-18 04:11:42 +0100

    update

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import SearchIcon from '../assets/search-icon.svg';
+import Bookstack from '../assets/book-stack.svg';
+
+const Hero = () => {
+  return (
+
+
+    <>
+        <div className='w-full h-128 flex pl-10'>
+
+            <div className='w-1/2 h-128'>
+
+                <h1 className='font-bold pt-40  text-6xl'>
+                    FIND AND BUY <br /> A BOOK
+                </h1>
+
+                <p className=' pt-3 text tracking-wide'> 
+                    find your favorite book and read it here for free
+                </p>
+
+                <div className="flex relative items-center pt-5">
+
+                <input type="search" className="w-96 h-10 bg-gray-100 placeholder:pl-10 text-xs" placeholder='Search Book' />
+                <img src={SearchIcon} alt="searchicon" className='absolute pl-2 h-5'/>
+
+                </div>
+
+            </div>
+
+            <div className='w-1/2 h-128 bg-orange-500 flex items-end'>
+            <img src={Bookstack} alt="books" className=''/>
+            </div>
+
+        </div>
+    </>
+  );
+};
+
+export default Hero;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import CartIcon from '../assets/cart-icon.svg';
+
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 bg-transparent p-4 w-full">
+      <div className="container mx-auto flex justify-between items-center">
+        <a className="text-black text-2xl font-medium" href="/">BOOKSTORE</a>
+        <ul className="flex space-x-4 text-black">
+          <li><a href="/">
+              <img src={CartIcon} alt="Carticon" />
+            </a></li>
+          <li><a href="/"><button className='bg-white px-5 py-1 rounded-xl font-medium'>Login</button></a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
