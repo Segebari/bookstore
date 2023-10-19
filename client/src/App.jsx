27a70c772d66fcb9d88a3bc2fcf6d6272b722b6c
Segebari <41949716+Segebari@users.noreply.github.com>
2023-10-19 13:01:11 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Books from './components/Books';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Books />
     </>
   )
 }
---
+++ client/src/components/Books.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Books = () => {
+  return (
+    <>
+      <section className='bg-gray-100 h-full p-56'>
+            <div className='flex gap-16'>
+
+                <div className='relative'>
+                    <div className='bg-white h-80 w-96'>
+                        <h1 className='text-black text-light'>All The Light We <br />
+                        Cannot See</h1>
+                    </div>
+                    <div className='absolute'>
+
+                    </div>
+                </div>
+
+                <div className='relative'>
+                <div className='bg-white h-80 w-96'>
+                        <h1 className='text-black text-light'>All The Light We <br />
+                        Cannot See</h1>
+                    </div>
+                    <div className='absolute'>
+
+                    </div>
+                </div>
+
+            </div>
+      </section>
+    </>
+  )
+}
+
+export default Books
