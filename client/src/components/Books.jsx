7bdc6cd5cbb3b8563a80518e0c7f246b5be2656f
Segebari <41949716+Segebari@users.noreply.github.com>
2023-10-19 14:38:56 +0100

    update

--- client/src/components/Books.jsx
+++ client/src/components/Books.jsx
@@ -7,9 +7,30 @@ const Books = () => {
             <div className='flex gap-16'>
 
                 <div className='relative'>
-                    <div className='bg-white h-80 w-96'>
+                    <div className='bg-white h-80 w-96 pl-28 pr-10 pt-8'>
                         <h1 className='text-black text-light'>All The Light We <br />
                         Cannot See</h1>
+                        <h5 className='text-gray-500'>By Anthony Doerr</h5>
+
+                        <div className='flex justify-between w-full'>
+
+                            <div className='flex gap-1'>
+                            <i class="uil uil-star"></i>
+                            <i class="uil uil-star"></i>
+                            <i class="uil uil-star"></i>
+                            <i class="uil uil-star"></i>
+                            <i class="uil uil-star"></i>
+                            </div>
+
+                            <p className='text-gray-300'> 500 voters </p>
+                        </div>
+
+                        <div className='text-gray-300 pt-6'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, enim quidem? Ab, mollitia.
+                        </div>
+
+                        <button className='w-full border border-orange-400 rounded-md mt-4 py-1 text-orange-400 bg-orange-100 hover:text-white hover:bg-orange-400 font-medium'> Buy Now </button>
+                        
                     </div>
                     <div className='absolute'>
 
